844010ed04ce233679efd6e0565ef2f8c1e95679
David E. Wheeler <david@justatheory.com>
2025-03-20 19:23:00 -0400

    Show output

---
+++ src/line/tests.rs
@@ -0,0 +1,49 @@
+use super::*;
+use std::str;
+
+#[test]
+fn line_writer() {
+    let mut buf = Vec::new();
+    let mut lw = LineWriter::new(&mut buf);
+    assert!(lw.write_line("hello").is_ok());
+    assert!(lw.write("yes".as_bytes()).is_ok());
+    assert!(lw.flush().is_ok());
+    let res = str::from_utf8(buf.as_slice()).unwrap();
+    assert_eq!("hello\nyes", res);
+
+    // Write a couple of lines with a boxed LineWriter.
+    let mut buf = Vec::new();
+    let mut lw = Box::new(LineWriter::new(&mut buf));
+    assert!(lw.write_line("🤘🏻 Rock on gold dust woman").is_ok());
+    assert!(lw.write_line("🐦‍⬛ Crows are rad.").is_ok());
+    let res = str::from_utf8(buf.as_slice()).unwrap();
+    assert_eq!("🤘🏻 Rock on gold dust woman\n🐦‍⬛ Crows are rad.\n", res);
+}
+
+#[test]
+fn color_line() {
+    let mut buf = Vec::new();
+    let style = Style::new().green();
+    let mut lw = ColorLine::new(&mut buf, style);
+    assert!(lw.write_line("hello").is_ok());
+    assert!(lw.write("yes".as_bytes()).is_ok());
+    assert!(lw.flush().is_ok());
+    let res = str::from_utf8(buf.as_slice()).unwrap();
+    assert_eq!(format!("{}\nyes", style.style("hello")), res);
+
+    // Write a couple of lines with a boxed LineWriter.
+    let mut buf = Vec::new();
+    let style = Style::new().red();
+    let mut lw = Box::new(ColorLine::new(&mut buf, style));
+    assert!(lw.write_line("🤘🏻 Rock on gold dust woman").is_ok());
+    assert!(lw.write_line("🐦‍⬛ Crows are rad.").is_ok());
+    let res = str::from_utf8(buf.as_slice()).unwrap();
+    assert_eq!(
+        format!(
+            "{}\n{}\n",
+            style.style("🤘🏻 Rock on gold dust woman"),
+            style.style("🐦‍⬛ Crows are rad.")
+        ),
+        res
+    );
+}
--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,35 +1,43 @@
 //! Build Pipeline interface definition.
 
-use crate::{error::BuildError, pg_config::PgConfig};
+use crate::exec::Executor;
+use crate::line;
+use crate::{error::BuildError, line::WriteLine, pg_config::PgConfig};
 use log::debug;
 use std::{io::Write, path::Path, process::Command};
 
 /// Defines the interface for build pipelines to configure, compile, and test
 /// PGXN distributions.
-pub(crate) trait Pipeline<P: AsRef<Path>> {
+pub(crate) trait Pipeline<
+    O = line::LineWriter<std::io::Stdout>,
+    E = line::LineWriter<std::io::Stderr>,
+> where
+    O: WriteLine,
+    E: WriteLine,
+{
     /// Creates an instance of a Pipeline.
-    fn new(dir: P, pg_config: PgConfig) -> Self;
+    fn new(exec: Executor, pg_config: PgConfig) -> Self;
 
     /// Returns a score for the confidence that this pipeline can build the
     /// contents of `dir`. A score of 0 means no confidence and 255 means the
     /// highest confidence.
-    fn confidence(dir: P) -> u8;
+    fn confidence(dir: impl AsRef<Path>) -> u8;
 
     /// Configures a distribution to build on a particular platform and
     /// Postgres version.
-    fn configure(&self) -> Result<(), BuildError>;
+    fn configure(&mut self) -> Result<(), BuildError>;
 
     /// Compiles a distribution on a particular platform and Postgres version.
-    fn compile(&self) -> Result<(), BuildError>;
+    fn compile(&mut self) -> Result<(), BuildError>;
 
     /// Installs a distribution on a particular platform and Postgres version.
-    fn install(&self) -> Result<(), BuildError>;
+    fn install(&mut self) -> Result<(), BuildError>;
 
     /// Tests a distribution a particular platform and Postgres version.
-    fn test(&self) -> Result<(), BuildError>;
+    fn test(&mut self) -> Result<(), BuildError>;
 
-    /// Returns the directory passed to [`new`].
-    fn dir(&self) -> &P;
+    /// Returns the Executor passed to [`new`].
+    fn executor(&mut self) -> &mut Executor;
 
     /// Returns the PgConfig passed to [`new`].
     fn pg_config(&self) -> &PgConfig;
@@ -53,7 +61,7 @@ pub(crate) trait Pipeline<P: AsRef<Path>> {
     /// Attempts to write a temporary file to `dir` and returns `true` on
     /// success and `false` on failure. The temporary file will be deleted.
     fn is_writeable<D: AsRef<Path>>(&self, dir: D) -> bool {
-        debug!(dir:display = crate::filename(&dir); "testing write access");
+        debug!(dir:? = crate::filename(&dir); "testing write access");
         match tempfile::Builder::new()
             .prefix("pgxn-")
             .suffix(".test")
@@ -66,7 +74,7 @@ pub(crate) trait Pipeline<P: AsRef<Path>> {
 
     /// Run a command. Runs it with elevated privileges when `sudo` is true
     /// and `pg_config --pkglibdir` isn't writeable by the current user.
-    fn run<S, I>(&self, program: &str, args: I, sudo: bool) -> Result<(), BuildError>
+    fn run<S, I>(&mut self, program: &str, args: I, sudo: bool) -> Result<(), BuildError>
     where
         I: IntoIterator<Item = S>,
         S: AsRef<std::ffi::OsStr>,
@@ -74,22 +82,9 @@ pub(crate) trait Pipeline<P: AsRef<Path>> {
         // Use `sudo` if the param is set.
         let mut cmd = self.maybe_sudo(program, sudo);
         cmd.args(args);
-        cmd.current_dir(self.dir());
-        match cmd.output() {
-            Ok(out) => {
-                if !out.status.success() {
-                    return Err(BuildError::Command(
-                        format!("{:?}", cmd),
-                        String::from_utf8_lossy(&out.stderr).to_string(),
-                    ));
-                }
-                Ok(())
-            }
-            Err(e) => Err(BuildError::Command(
-                format!("{:?}", cmd),
-                e.kind().to_string(),
-            )),
-        }
+
+        // Execute the command.
+        self.executor().execute(cmd)
     }
 }
 
